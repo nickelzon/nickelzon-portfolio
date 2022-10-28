8a6bade9d26094570006af13686a48fc235391d2
alnickelzon <al.villanueva1234@gmail.com>
2022-10-28 11:45:49 +0800

    screen size for freakin responsiveness and fonts too

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+      fontFamily: {
+        questrial: ["Questrial"],
+      },
+    },
   },
   plugins: [],
-}
+};
