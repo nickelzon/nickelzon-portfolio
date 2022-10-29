94c1392843918eb322545d744e2f4d1612957afc
alnickelzon <al.villanueva1234@gmail.com>
2022-10-29 18:33:51 +0800

    fixed picture in hero sec

--- src/Components/Education.tsx
+++ src/Components/Education.tsx
@@ -7,7 +7,7 @@ const Education = ({ sheesh }: Props) => {
 
   return (
     <div className="hero flex flex-col flex-wrap items-center p-5 font-questrial">
-      <span className="mt-10 text-2xl font-extrabold text-orange-500">
+      <span className="opacity mt-10 text-2xl font-extrabold text-orange-500">
         Education
       </span>
       <span className="opacity text-lg text-orange-500">
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,19 +1,20 @@
+import { authorphoto } from "../Assets";
+
 const Hero = () => {
   return (
-    <div className="hero flex justify-center font-questrial">
-      <div className="flex flex-col items-center justify-center text-orange-500 ss:flex-row">
-        <div
-          className={`author h-96 w-[220px] xs:w-[250px] ss:w-[350px] sm:w-[650px]`}
-        />
-        <div className="max-w-sm p-4">
-          <h1 className="pulse text-4xl font-black text-white">
-            I am Al Nickelzon.
-          </h1>
-          <p className="animation-in-3s text-xl">
-            Self taught React Developer. Graphic Designer. Over-caffeinated
-            human.
-          </p>
-        </div>
+    <div className="hero flex flex-col items-center justify-center p-20 font-questrial text-orange-500 sm:flex-row">
+      <img
+        src={authorphoto}
+        alt={authorphoto}
+        className="author mb-10 max-w-[250px] rounded-full ss:max-w-[450px] sm:mb-0 sm:mr-10"
+      />
+      <div className="flex flex-col justify-center">
+        <h1 className="pulse text-4xl font-black text-white">
+          I am Al Nickelzon.
+        </h1>
+        <p className="animation-in-3s text-xl">
+          Self taught React Developer. Graphic Designer. Over-caffeinated human.
+        </p>
       </div>
     </div>
   );
