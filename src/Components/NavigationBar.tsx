a29d11245b0029a782e044f3a783f861e0bf44cb
alnickelzon <al.villanueva1234@gmail.com>
2023-01-09 21:17:54 +0800

    links in navlinks as functional component(no mobile-design yet)

--- src/Components/NavigationBar.tsx
+++ src/Components/NavigationBar.tsx
@@ -1,27 +1,47 @@
 import NavLinks from "../Links/index";
 import { menuopen, menuclose, logo } from "../Assets/index";
-import { useState } from "react";
+import { useState, FC } from "react";
+
+type linksProp = {
+  margin: string;
+  lastItemMargin: string;
+  mediaScreen: string;
+};
+
+const Links: FC<linksProp> = ({ margin, lastItemMargin, mediaScreen }) => {
+  type navlinks = { id: number; title: string; url: string };
+  const style = {
+    ul: `items-center ${mediaScreen}`,
+  };
+
+  return (
+    <ul className={style.ul}>
+      {NavLinks.map((links: navlinks, index: number) => (
+        <li
+          key={links.id}
+          className={`navlinks ${
+            index === NavLinks.length - 1 ? lastItemMargin : margin
+          }`}
+        >
+          <a href={links.url}>{links.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 const NavigationBar = () => {
-  type navlinks = { id: number; title: string; url: string };
   const [hamburger, toggleHamburger] = useState(false);
 
   return (
     <div className="bg-orange-700 p-5 font-questrial text-white">
       <div className="flex justify-between">
         <img src={logo} alt="logo" className="white max-w-[50px]" />
-        <ul className="hidden items-center ss:flex">
-          {NavLinks.map((links: navlinks, index: number) => (
-            <li
-              key={links.id}
-              className={`navlinks ${
-                index === NavLinks.length - 1 ? "mr-0" : "mr-4"
-              }`}
-            >
-              <a href={links.url}>{links.title}</a>
-            </li>
-          ))}
-        </ul>
+        <Links
+          mediaScreen="hidden ss:flex"
+          margin="mr-4"
+          lastItemMargin="mr-0"
+        />
         <img
           src={!hamburger ? menuopen : menuclose}
           alt={menuopen}
@@ -29,20 +49,6 @@ const NavigationBar = () => {
           onClick={() => toggleHamburger(!hamburger)}
         />
       </div>
-      <ul
-        className={`animation-in ${
-          hamburger ? "flex" : "hidden"
-        } mt-10 flex-col text-center ss:hidden`}
-      >
-        {NavLinks.map((links: navlinks, index: number) => (
-          <li
-            key={links.id}
-            className={`${index === NavLinks.length - 1 ? "mb-0" : "mb-4"}`}
-          >
-            <a href={links.url}>{links.title}</a>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
